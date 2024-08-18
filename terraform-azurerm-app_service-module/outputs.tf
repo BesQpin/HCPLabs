239545acc951558dad8796f71218d30c5034b3c9
Peter Williams <25635012+BesQpin@users.noreply.github.com>
2024-08-18 21:52:11 +0100

    added files

--- terraform-azurerm-app_service-module/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "app_service_default_hostname" {
-  description = "The default hostname of the App Service"
-  value       =  values(azurerm_linux_web_app.app).*.default_hostname
-
-}
--- terraform-azurerm-resource_group-module/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "name" {
-  value = azurerm_resource_group.rg1.name
-}
-
-output "location" {
-  value = azurerm_resource_group.rg1.location
-}
